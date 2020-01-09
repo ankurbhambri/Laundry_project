a8c215513e4fd7b651b90e5b8fe5fd2b6c1a441d
ankuroo7 <ankurwave09@gamil.com>
2020-01-09 18:04:09 +0530

    old requirements done

--- app1/views.py
+++ app1/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from .models import Customer, Product, Order
 from django.views.generic import ListView
+from tabulate import tabulate
 import random
 import string
 import json
@@ -37,22 +38,25 @@ def contact(request):
         totalCount = request.GET.get('totalCount')
         totalamount = request.GET.get('totalCart')
         cartArray = request.GET.get('cartArray')
-        Order.objects.create(
+        cart = json.loads(cartArray)
+        row = [x.values() for x in cart]
+        order = Order.objects.create(
             customer_name=name,
             customer_email=email,
-            query_json=cartArray,
+            query_json=cart,
             total_quantity=totalCount,
             total_price=totalamount,
         )
-        # if likedpost:
-        #     from_email = settings.EMAIL_HOST_USER
-        #     recipient_list = [email]
-        #     send_mail(
-        #         'CompanyName Laundry Service',
-        #         ' Hi, ' + name +
-        #         '\n Thank you for choosing us your reference id is ' + order_id_gen +
-        #         '\n We are processing your request and our team will contact you soon.',
-        #         from_email,
-        #         recipient_list, fail_silently=False
+        if order:
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+            send_mail(
+                'CompanyName Testing Laundry Services',
+                ' Hi, ' + name +
+                '\n Thank you for choosing us your order id is ' + str(order.id) +
+                '\n' + tabulate(row, headers=['Items', 'Price', 'Count']) +
+                '\n Total of your order : ' + totalamount,
+                from_email,
+                recipient_list, fail_silently=False)
     return render(request, 'app1/base.html', {'prod': prod})
     
